cfd6a382e3aea4dbee3db19f5d3bc60d0aeb4634
gitfilix <runlevel07@gmail.com>
2020-05-09 13:52:46 +0200

    type annotations examples

--- features/annotations/vairables.ts
+++ features/annotations/vairables.ts
@@ -1,3 +1,4 @@
+// w
 // type annotation
 let apples: number = 5
 let speed: string = 'fast'
@@ -8,3 +9,35 @@ let nothing: undefined = undefined
 
 // built in objects
 let now: Date = new Date;
+
+// complex annotations (Object Literal Annotations)
+// -------------------------------------------------
+
+// Array with strings 
+let colors: string[] = ['red', 'green', 'car']
+let nyNumbers: number[] = [1, 44, 234]
+let truths: boolean[] = [true, true, false]
+
+
+// Classes: variable car is refering to an Instance of class Car 
+class Car {
+   
+}
+let car: Car = new Car()
+
+// Object literal: properties assignent
+let point: {x: number; y: number} = {
+  x: 12,
+  y: 32
+}
+
+
+// Function 
+const logNumber: (i: number) => void = (i: number ) => {
+  console.log('i:', i)
+}
+
+
+
+
+
