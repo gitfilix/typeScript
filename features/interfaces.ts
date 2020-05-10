5d35fd55dd1a21d83c2ec825e09bfee3ec31bd1c
gitfilix <runlevel07@gmail.com>
2020-05-10 15:05:34 +0200

    all important interface

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,14 +1,28 @@
-// 
-const oldCivic = {
-  name: 'civic',
-  year: 2000,
-  broken: true
+// type interface Vehicle - very generic
+
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string; // summary is a function that returns a string
 }
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean}): void => {
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+}
+
+// to call printVehicle you must pass an object that meets the specification defined in a Interface
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken?: ${vehicle.broken}`); 
+  console.log(`Broken?: ${vehicle.broken}`);
+  console.log(vehicle.summary());
+  
 }
 
 printVehicle(oldCivic)
