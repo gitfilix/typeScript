38e00b08bc6beb6eb04f7b1af2335eaec29c7cff
gitfilix <runlevel07@gmail.com>
2020-05-10 20:54:47 +0200

    google api maps works

--- maps-app/src/index.ts
+++ maps-app/src/index.ts
@@ -2,10 +2,14 @@
 import { User } from './User'
 import { Company } from './Company'
 
-const user = new User();
+// const user = new User();
 const company = new Company();
-
+// 
 console.log(company);
-console.log(user);
+// console.log(user);
+const mapDiv = document.getElementById('map') as HTMLElement;
 
-google
+new google.maps.Map(mapDiv, {
+  zoom: 1,
+  center: {lat: 0, lng: 0 },
+})
