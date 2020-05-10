9226d0c0eddd7142b78bc982e5371f78a5565a7f
gitfilix <runlevel07@gmail.com>
2020-05-10 18:48:57 +0200

    implement google api key and type-file for it

--- maps-app/src/index.ts
+++ maps-app/src/index.ts
@@ -1,7 +1,11 @@
+/// <reference types="@types/googlemaps" />
 import { User } from './User'
 import { Company } from './Company'
+
 const user = new User();
 const company = new Company();
 
 console.log(company);
 console.log(user);
+
+google
