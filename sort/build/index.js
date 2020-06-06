d3ab4ed9030eda022c36ff1474ee2b46de91ab05
gitfilix <runlevel07@gmail.com>
2020-06-06 12:42:31 +0200

    sort: bubblesort array of numbers with constructor

--- sort/build/index.js
+++ sort/build/index.js
@@ -4,6 +4,23 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
+    Sorter.prototype.sort = function () {
+        // lenght of the array
+        var length = this.collection.length;
+        // implementation of bubble-sort (double-loop iterate ) here:
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                // if leftHand-side of two elements is bigger -> swap it to the right
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
     return Sorter;
 }());
-var sorter = new Sorter([10, 3, -5, 0]);
+var myListToSort = new Sorter([10, 3, -5, 0, 7, 33, -7]);
+myListToSort.sort();
+console.log(myListToSort.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,10 +1,27 @@
 // sort project
 
 class Sorter {
-  constructor(public collection: number[]) {
+  constructor(public collection: number[]) {}
+  sort(): void {
+    // lenght of the array
+    const { length } = this.collection; 
+    
+    // implementation of bubble-sort (double-loop iterate ) here:
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length -i - 1; j ++) {
+        // if leftHand-side of two elements is bigger -> swap it to the right
+        if (this.collection[j] > this.collection[ j +1 ]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[ j +1 ];
+          this.collection[ j +1 ] = leftHand;
+        }
+      }
+    }
+        
   }
-
 }
 
-const sorter = new Sorter([10, 3, -5, 0]);
+const myListToSort = new Sorter([10, 3, -5, 0, 7, 33, -7]);
+myListToSort.sort()
 
+console.log(myListToSort.collection);
