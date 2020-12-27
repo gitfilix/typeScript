305f9206500cb30ba3e946975602c5110a517a82
gitfilix <runlevel07@gmail.com>
2020-12-27 16:06:29 +0100

    event handling and callback

--- webFramework/src/models/User.ts
+++ webFramework/src/models/User.ts
@@ -1,20 +1,34 @@
 // type definition
 interface UserProps {
-  name: string; 
-  age: number; 
+  name?: string; 
+  age?: number; 
 }
 
+// Type Alias: function has no input and no cb
+type Callbackfn = () => {}
+
 export class User {
+  // events annotation: 
+  // keys: strings,
+  // callbackfn in a Array of Cbfn
+  // initialized with empty objece 
+  events: { [key: string]: Callbackfn[] } = {};
+
   constructor (private data: UserProps){}
 
   //get: either name or age could be passed: 
   get(propName: string): (number | string) {
     return this.data[propName];
   }
-  
+
   // : void nothing will be returned
   set(update: UserProps): void {
     Object.assign(this.data, update)
+  }
+
+  //event handler: event and callback-fn
+  on(eventName: string, callback: Callbackfn) {
+
   }
 
 }
