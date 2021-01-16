a324de40cbf0b18a64fbdba44bd4a6db0d3b54ec
gitfilix <runlevel07@gmail.com>
2021-01-16 18:49:30 +0100

    function annotation part 2

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -4,3 +4,40 @@ const add = (a :number, b: number): number => {
   return a + b
 }
 
+const subtract = (a :number, b :number) :number => {
+  return a - b
+}
+
+// old syntax: named function
+function divide(a: number, b: number): number {
+  return a / b 
+}
+
+// anonymous function 
+const multiply = function(a: number, b: number): number {
+  return a * b
+}
+
+// will never return anything therefore: void
+const logger = (message: string): void => {
+  console.log(message)
+}
+
+const throwError = (message: string): void => {
+  if(!message) {
+    throw new Error(message)
+  }
+}
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'raining'
+}
+
+const logWeather = ({ date, weather }: { date: Date, weather: string}): void => {
+  
+  console.log(date)
+  console.log(weather)
+}
+
+logWeather(todaysWeather)
